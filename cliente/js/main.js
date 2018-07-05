e18be42a6e1816ebca1d39e93316422471d20fda
bap17 <bap14@alu.ua.es>
2018-07-05 21:32:39 +0200

    Inicio de cliente estatico

--- cliente/js/main.js
+++ cliente/js/main.js
@@ -1,16 +1,12 @@
-//Este es el "archivo inicial" tal y como está configurado en webpack
-//La ejecución de js comenzaría por aquí.
-//importamos los componentes y los pintamos en el HTML con ReactDOM.render
-
 import React from 'react'
 import ReactDOM from 'react-dom'  
-//Al ser un export default, al importarlo podemos cambiarle el nombre
-//import Saludo from './ComponenteSaludo'
+
 import Connect from './ComponenteConnect'
+import Inicio from './ComponenteInicio'
 
 /*ReactDOM.render(<Saludo nombre="Pepe"/>
             , document.getElementById('componente'));  */
 
-ReactDOM.render(<Connect />, document.getElementById('componente')); 
+//ReactDOM.render(<Connect />, document.getElementById('componente')); 
 
-
+ReactDOM.render(<Inicio />, document.getElementById('componente')); 
